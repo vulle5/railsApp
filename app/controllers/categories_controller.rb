07edad2f3639a77801a9e03a92660ba0a37e8433
Severi Tikkanen <severi.tikkanen@taikala.fi>
2019-12-27 14:31:28 +0200

    Added admin requirement to categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
@@ -23,6 +25,13 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    return if logged_in? && current_user.admin?
+
+    flash[:danger] = 'Only admins can create categories'
+    redirect_to categories_path
   end
 
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @category = Category.create(name: 'sports')
+    @user = User.create(username: 'John', email: 'john@example.com',
+                        password: 'password', admin: true)
   end
 
   test 'should get categories index' do
@@ -11,6 +13,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get new' do
+    sign_in_as(@user, 'password')
     get new_category_path
     assert_response :success
   end
@@ -18,5 +21,12 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test 'should get show' do
     get category_path(@category)
     assert_response :success
+  end
+
+  test 'should redirect to create when not admin' do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: 'sports' } }
+    end
+    assert_redirected_to categories_path
   end
 end
