46ceab753a72e7e4fd4bfdd6d6c3eb20f309dbea
Severi Tikkanen <severi.tikkanen@taikala.fi>
2019-12-27 14:58:43 +0200

    Added article_category to db

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ActiveRecord::Base
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, presence: true, uniqueness: true, length: { maximum: 25, minimum: 3 }
 end
