4a9ed7b889723679405607033898950c61db43b9
Severi Tikkanen <severi.tikkanen@taikala.fi>
2019-12-27 15:34:30 +0200

    Added categories to all views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,10 @@ class CategoriesController < ApplicationController
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
-  def show; end
+  def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
 
   def new
     @category = Category.new
